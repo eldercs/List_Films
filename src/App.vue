e9f64fb7bae8cf7d91e7fa2ec24c5d9cef7a71b5
eldercs <deny_1999@ukr.net>
2022-09-04 14:54:27 +0300

    first commit

--- src/App.vue
+++ src/App.vue
@@ -12,6 +12,7 @@ export default {
 }
 </script>
 <style lang="scss">
+@import "../src/assets/fonts.css";
 #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
--- vue-config.js
+++ vue-config.js
@@ -1,5 +1,5 @@
 module.exports = {
     publicPath: process.env.NODE_ENV === 'production'
-    ? '/Sort_Films/'
+    ? '/List_Films/'
     : '/'
     }
