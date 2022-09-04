86b91346816ddc6d5820af549d1f189cf0299d1b
eldercs <deny_1999@ukr.net>
2022-09-04 15:29:27 +0300

    first commit

--- src/App.vue
+++ src/App.vue
@@ -12,7 +12,7 @@ export default {
 }
 </script>
 <style lang="scss">
-@import "../src/assets/fonts.css";
+@import "./fonts.css";
 #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
